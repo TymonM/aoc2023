9cd2ba6ca8d5418605fa4329c25c71c1b3495174
tymon <tymon.mieszkowski@gmail.com>
2023-12-10 21:43:06 +1300

    day 6

---
+++ src/bin/06/second.rs
@@ -0,0 +1,14 @@
+pub fn run(input: &str) {
+    let time = input.lines().nth(0).unwrap()
+        .split_whitespace()
+        .collect::<Vec<&str>>()[1..]
+        .join("")
+        .parse::<i64>().unwrap();
+    let distance = input.lines().nth(1).unwrap()
+        .split_whitespace()
+        .collect::<Vec<&str>>()[1..]
+        .join("")
+        .parse::<i64>().unwrap();
+
+    println!("{}", ((time as f32 * time as f32 / 4f32 - (distance+1) as f32).sqrt() + 0.5f32) as i64 * 2 - (time+1) % 2);
+}
